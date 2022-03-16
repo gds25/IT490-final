91a76358f0c5cd17aa52a5c324e8b929eed9406e
db488 <db488@njit.edu>
2022-03-16 09:00:06 -0400

    added anime table

--- SQLFiles/database.sql
+++ SQLFiles/database.sql
@@ -19,4 +19,14 @@ CREATE TABLE IF NOT EXISTS Shows (
 	Primary Key(ID)
 );
 
+CREATE TABLE IF NOT EXISTS anime (
+	mal_id varchar(255) NOT NULL, 
+	title varchar(255) NOT NULL, 
+	img varchar(255) NOT NULL, 
+	rating varchar(255) DEFAULT NULL, 
+	genre varchar(255) DEFAULT NULL, 
+	trailer varchar(255) DEFAULT NULL, 
+	synopsis varchar(255) DEFAULT NULL
+);
+
 
