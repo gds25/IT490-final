241504b3524db3b1a529c92221bfab8f91cdddca
Baljot Singh <77807137+bs526@users.noreply.github.com>
2022-03-18 02:41:34 -0400

    Part 1/3 of user profile recommendations

---
+++ getUser.php
@@ -0,0 +1,24 @@
+<?php
+
+$request = new HttpRequest();
+$request->setUrl('Refresh: .1; url=getUser.php');
+$request->setMethod(HTTP_METH_POST);
+
+$request->setHeaders([
+	'content-type' => 'application/x-www-form-urlencoded',
+	'x-rapidapi-host' => 'Anilistmikilior1V1.p.rapidapi.com',
+	'x-rapidapi-key' => 'b7cc2bd809msh30912c0d897635dp1ef909jsnd949b6cad880'
+]);
+
+$request->setContentType('application/x-www-form-urlencoded');
+$request->setPostFields([
+	'accessToken' => '<REQUIRED>'
+]);
+
+try {
+	$response = $request->send();
+
+	echo $response->getBody();
+} catch (HttpException $ex) {
+	echo $ex;
+}
