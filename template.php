23599c6aa0c2dc7d26a3d98a21e2db55613db4e8
db488 <db488@njit.edu>
2022-03-19 21:41:31 -0400

    fixed rating system

--- template.php
+++ template.php
@@ -23,7 +23,7 @@
 //https://stackoverflow.com/questions/426310/how-do-you-post-data-with-a-link
 
 //temp for me
-$id = 48583;
+$id = $_GET['id'];
 
 //Set up SQL Server info
 $servername = "127.0.0.1";
@@ -58,13 +58,39 @@ $mal_id = $anime['mal_id'];
 $img = $anime['img'];
 $title = $anime['title']; 
 $trailer = $anime['trailer'];
+$genres = $anime['genre'];
 $rating = $anime['rating'];
 $synopsis = $anime['synopsis'];
+$userRatings = $anime['userRatings'];
+
+$genres = substr($genres, 2, -2);
+$genres = str_replace("\",\"", ", ", $genres);
+
+ //updating user rating
 
 
- 
-
- 
+if(isset($_POST['upvote'])) {
+  unset($_POST['upvote']);
+  changeRating(1);
+}
+else if(isset($_POST['downvote'])) {
+  unset($_POST['downvote']);
+  changeRating(-1);
+}
+function changeRating($value) {
+  global $servername, $username, $password, $dbname, $userRatings, $id;
+  $userRatings += $value;
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  $stmt = $conn->prepare("UPDATE anime SET userRatings=$userRatings WHERE mal_id = $id");
+  $stmt->execute();
+  if (isset($_POST['upvote'])){
+    echo '
+    <script type="text/javascript">
+    location.reload();
+    </script>';
+    }
+  $conn->close();
+}
 ?>
 <html>
 <head>
@@ -82,7 +108,6 @@ $synopsis = $anime['synopsis'];
 
 <h1 align="center">Anime Database</h1>
 
-
  
 
 <div class="row">
@@ -91,7 +116,8 @@ $synopsis = $anime['synopsis'];
   </div>
   <div class="column">
     <h2><?php echo $title ?></h2>
-    <p><b>Synopsis</b>: <?php echo $synopsis ?>...</p>
+    <p><b>Synopsis</b>: <?php echo $synopsis ?></p>
+    <p><b>Genres</b>: <?php print_r($genres) ?></p>
     <p><b>Rating</b>: <?php echo $rating ?></p>
     <p><b>Watch the trailer <a href=<?php echo $trailer ?> target="_blank" rel="noopener noreferrer">Here</a></b> </p>
 
@@ -103,9 +129,10 @@ $synopsis = $anime['synopsis'];
 </div>
 <br>
 <div class="reviews">
-    <h3>Did you watch this?</h3>
+  <h3>Community Rating: <?php echo $userRatings ?></h3>
+    <h4>Did you watch this?</h4>
     <p>Leave a review for others</p>
-<<<<<<< HEAD
+
     
     
 
