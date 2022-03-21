3dc7e9517b5ed22ab77d7d166f4d3e4028553751
Baljot Singh <bs526@njit.edu>
2022-03-21 14:42:05 -0400

    Removed the completed (example) directory; Added new push file.

---
+++ pushnotifications.php
@@ -0,0 +1,29 @@
+<?php
+    // database credentials
+    $servername = "";
+    $username = "";
+    $password = "";
+    $dbname = "users";
+
+    // create connection with given credentials
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //check connection
+    if ($conn->connect_error){
+        //kill connection + show on result
+        die("connection failed: " . $conn->connect_error);
+    }
+    
+    //GET TARGET USER ID FROM SOMEWHERE
+    //$targetID = $_POST['somename'];
+
+    $notificationenable = "UPDATE users
+            SET notif = 1
+            WHERE ID = $targetID";
+    $notificationdisable = "UPDATE users
+            SET notif = 0
+            WHERE ID = $targetID";
+
+    $result = $conn->query($notificationdisable);
+    
+?>
